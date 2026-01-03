16768022f0aefa192b58888fb517d48867a7e9c3
JasrajSingh-7 <jasrajsinghtailor7@gmail.com>
2026-01-03 15:58:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Articles from "./pages/Articles";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
